135803129d4aa6788b8e6d483c54bdfb9bb2e6fc
Vince Carey <stvjc@channing.harvard.edu>
2006-08-11 20:17:08 +0000

    formula interface in oneFit
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@19155 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/oneFit.R
+++ R/oneFit.R
@@ -60,3 +60,33 @@ setMethod("oneFit", c("racExSet", "list", "character", "function"),
 		as.character(snpid)))
     fitfun(fmla, data=ndf)
 })
+
+setMethod("oneFit", c("racExSet", "list", "formula", "function"),
+  function(racExSet, geneid, snpid, fitfun, ...) {
+#    fixFmla = function(x) {
+#      x = as.character(x)
+#      x = x[x != "~"]
+#      x = strsplit(x, "\\+")
+#      extractVars = function(x) gsub(" ","",gsub(")","", gsub("factor\\(","",x)))
+#      as.character(sapply(x, extractVars))
+#      }
+    if (length(as.list(snpid))>2) stop("formula must be of form ~..., no dep. var.")
+    snpvars = all.vars(snpid)
+    snpvals = snps(racExSet)[snpvars,]
+    if (length(snpvars)>1) snpvals=t(snpvals)
+    exvals = geneid
+    if (length(geneid) > 1) stop("geneid must be list of length 1")
+    geneName = names(geneid)
+    if (length(geneName) == 0) stop("geneid must have names attr")
+    exvals = geneid[[1]]
+    if (!is.numeric(exvals)) stop("geneid list content must be numeric")
+    geneid = geneName
+## as before below
+    if (length(exvals) != length(snpvals)) stop("lengths of numeric expression data and snp allele counts do not agree")
+    ndf = data.frame(exvals,snpvals)
+    names(ndf) = c(geneid, snpvars)
+# now build the formula required by fitfun out of the formula fragment and depvar name
+    snpid[[3]] = snpid[[2]]
+    snpid[[2]] = as.name(substitute(geneid))
+    fitfun(snpid, data=ndf)
+})
