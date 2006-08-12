d86f5201a64e28fde9769de1237f33e4af076510
Vince Carey <stvjc@channing.harvard.edu>
2006-08-12 19:14:15 +0000

    sliding out the exprSet basis
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@19168 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/detScreen.R
+++ R/detScreen.R
@@ -13,28 +13,44 @@ par(opar)
 invisible(list(bot=bot, cpn=cpn))
 }
 
+setClass("snpScreenResult", representation(call="call", 
+    locs="numeric", chr="character", fittertok="character"), contains="list")
+
 setGeneric("snpScreen", function(racExSet, snpMeta, gene, formTemplate, fitter, gran, ...)
     standardGeneric("snpScreen"))
 setMethod("snpScreen", c("racExSet", "snpMeta", "genesym", "formula", "function", "numeric"),
     function(racExSet, snpMeta, gene, formTemplate=~., fitter=lm, gran=1, ...) {
-      runTemplate = function(x,y) { 
-       # assuming that a template of the form ~. or ~factor(.) is supplied
-       # this replaces . by string y
-          uu = as.name(gsub("\\.", x[[2]], y))
-          x[[2]] = substitute(uu)
-          x
+      runTemplate = function(x,y) {
+       z = as.character(x)
+       z[2] = gsub("\\.", y, z[2])
+       as.formula(z)
       }
       psid = getpsid( gene, annotation(racExSet) )
       y = exprs(racExSet)[psid,]
       outco = list(y)
       names(outco) = as.character(gene)
       nsnp = length(sn <- snpNames(racExSet))
-      snpstodo = sn[ seq(1, nsnp, gran) ]
+      snpstodo = sn[ inuse <- seq(1, nsnp, gran) ]
+      locs = snpMeta[sn, "pos", drop=TRUE]
       out = list()
       for (i in 1:length(snpstodo)) {
+        if (options()$verbose == TRUE) {
+           if (i %% 100 == 0) cat(i)
+           }
         fm = runTemplate( formTemplate, snpstodo[i] ) 
-        out[[i]] = oneFit(racExSet, outco, fm, fitter)
+        out[[i]] = try(oneFit(racExSet, outco, fm, fitter))
       }
-      out
+      callsave = match.call()
+      fittertok = deparse(substitute(fitter))
+      new("snpScreenResult", call=callsave, 
+        locs=locs, chr=chromosome(snpMeta), fittertok=fittertok, out)
+})
+
+setMethod("show", "snpScreenResult", function(object) {
+   cat("GGtools snpScreenResult for call:\n")
+   print(object@call)
+   cat("There were", nf <- length(object), "attempted fits,\n")
+   nerr = sum(sapply(object, function(x) inherits(x, "try-error")))
+   cat("and", nf, "were successful.\n")
 })
 
