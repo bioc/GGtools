24c4b6fc7ca24301dff226f7f4ee72f2dfe4166a
Vince Carey <stvjc@channing.harvard.edu>
2006-08-13 05:06:00 +0000

    thinHM2rac added
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@19176 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/HM2rac.R
+++ R/HM2rac.R
@@ -1,5 +1,6 @@
 HM2rac = function (fn, comment.char = "", kppref = "NA") 
 {
+# SLOW!  see thinHM2rac below
     df = read.table(fn, comment.char = comment.char, h = TRUE)
     allel = as.character(df[, "SNPalleles"])
     kpcol = grep(kppref, names(df))
@@ -22,3 +23,35 @@ HM2rac = function (fn, comment.char = "", kppref = "NA")
     adf = new("AnnotatedDataFrame", data=vdf, varMetadata=vmdf)
     list(raremat = t(rr), alleles=allel, rareallele = rrr, anno=adf)
 }
+
+
+thinHM2rac = function(gzfn) {
+ stats = system(paste("gzcat", gzfn, "|wc"), intern=TRUE)
+ stats = as.numeric(strsplit(stats, "\ +")[[1]])
+ ntokpline = stats[3]/stats[2]
+ nline = stats[2]-1 # header
+ nind = ntokpline - 11
+ rarecount = matrix(NA, nr=nline, nc=nind)
+ alleles = rep(NA, nline)
+ rarebase = rep(NA, nline)
+ rsnum = rep(NA, nline)
+ ff = gzfile(gzfn)
+ open(ff, "r")
+ hd = scan(ff, "", n=ntokpline, quiet=TRUE)
+ snames = hd[-c(1:11)]
+ cat(paste(nline, "lines to process\n"))
+ for (i in 1:nline)
+   {
+   if (i %% 500 == 0) cat(i)
+   tmp = scan(ff, "", n=ntokpline, quiet=TRUE)
+   rarebase[i] = getRare(tmp[-c(1:11)])
+   rarecount[i,] = countRare(tmp[-c(1:11)])
+   rsnum[i] = tmp[1]
+   alleles[i] = tmp[2]
+   }
+ rownames(rarecount) = rsnum
+ colnames(rarecount) =  snames
+ list(rarecount=rarecount, rarebase=rarebase, 
+       alleles=alleles)
+}
+ 
