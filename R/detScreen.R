b33302ab47576a11fb2cdc235b13a7f9041213d6
Vince Carey <stvjc@channing.harvard.edu>
2006-08-16 05:16:59 +0000

    some C code added for additive genetic model
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@19249 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/detScreen.R
+++ R/detScreen.R
@@ -13,13 +13,13 @@
 #invisible(list(bot=bot, cpn=cpn))
 #}
 
-setClass("snpScreenResult", representation(call="call", gene="character",
+setClass("snpScreenResult", representation(call="call", gene="character", 
     locs="numeric", chr="character", fittertok="character"), contains="list")
 
 setGeneric("snpScreen", function(racExSet, snpMeta, gene, formTemplate, fitter, gran, ...)
     standardGeneric("snpScreen"))
 setMethod("snpScreen", c("racExSet", "snpMeta", "genesym", "formula", "function", "numeric"),
-  function (racExSet, snpMeta, gene, formTemplate = ~., fitter = lm, 
+   function (racExSet, snpMeta, gene, formTemplate = ~., fitter = lm, 
       gran = 1, ...) 
   {
       runTemplate = function(x, y) {
@@ -39,7 +39,21 @@ setMethod("snpScreen", c("racExSet", "snpMeta", "genesym", "formula", "function"
       names(allpos) = allsn
       snpstodo = intersect(snpstodo, allsn)
       locs = allpos[snpstodo]
+      fittertok = deparse(substitute(fitter))
+      callsave = match.call()
       out = list()
+      if (fittertok == "fastAGM") {
+        tmp = snps(racExSet)[snpstodo,]
+        bad = apply(tmp,1,function(x) any(is.na(x)))
+        if (any(bad)) {
+           warning("some genotype results had missing values; associated SNPs are dropped completely in this version when fastAGM is used.")
+           snpstodo = snpstodo[-which(bad)]
+           }
+        locs = allpos[snpstodo]
+        ans = fastAGM(snps(racExSet)[snpstodo,], y)
+        return(new("snpScreenResult", call=callsave, locs=locs, 
+            chr=chromosome(snpMeta), fittertok=fittertok, gene=as.character(gene), ans))
+      }
       for (i in 1:length(snpstodo)) {
           if (options()$verbose == TRUE) {
               if (i%%100 == 0) 
@@ -49,8 +63,6 @@ setMethod("snpScreen", c("racExSet", "snpMeta", "genesym", "formula", "function"
           out[[i]] = try(oneFit(racExSet, outco, fm, fitter))
       }
       names(out) = snpstodo
-      callsave = match.call()
-      fittertok = deparse(substitute(fitter))
       new("snpScreenResult", call = callsave, locs = locs, chr = chromosome(snpMeta), 
         fittertok = fittertok, gene=as.character(gene), out)
 })
@@ -58,26 +70,36 @@ setMethod("snpScreen", c("racExSet", "snpMeta", "genesym", "formula", "function"
 setMethod("show", "snpScreenResult", function(object) {
    cat("GGtools snpScreenResult for call:\n")
    print(object@call)
-   cat("There were", nf <- length(object), "attempted fits,\n")
-   nerr = sum(sapply(object, function(x) inherits(x, "try-error")))
-   cat("and", nf, "were successful.\n")
+   if (object@fittertok  != "fastAGM") {
+     cat("There were", nf <- length(object), "attempted fits,\n")
+     nerr = sum(sapply(object, function(x) inherits(x, "try-error")))
+     cat("and", nf-nerr, "were successful.\n")
+   }
+   else {
+     cat("There were", nf <- length(object[[4]]), "attempted fits,\n")
+     nerr = sum(is.na(object[[4]])) + sum(is.nan(object[[4]]))
+     cat("and", nf-nerr, "were successful.\n")
+   }
 })
 
 extract_p = function(ssr) {
+  if (ssr@fittertok == "fastAGM") return(ssr[["pval"]])
   if (ssr@fittertok != "lm") stop("code is idiosyncratic for lm fits")
   ps = as.numeric(sapply(ssr, function(x) try(summary(x)$coef[2,4],silent=TRUE)))
 }
 
-plot_mlp <- function (ssr, snpMeta, ps=NULL) 
+plot_mlp <- function (ssr, snpMeta, ps=NULL, pch=20, cex=1) 
 {
-    if (ssr@fittertok != "lm") 
+    if (ssr@fittertok == "fastAGM") ps = ssr[["pval"]]
+    else if (ssr@fittertok != "lm") 
         stop("code is idiosyncratic for lm fits")
     if (is.null(ps)) ps = as.numeric(sapply(ssr, function(x) try(summary(x)$coef[2, 
         4], silent = TRUE)))
-    if (length(ssr@locs) > 200) plotf = smoothScatter
+    if (length(ssr@locs) > 200) plotf = .smoothScatter2
      else plotf = plot
-    plotf(ssr@locs, -log10(ps), xlab = "chromosomal location", 
-        ylab = "-log10 p Ho:Bs=0", main = ssr@gene, xlim=range(snpMeta[,"pos"]))
+    bad = which(is.na(ps))
+    plotf(ssr@locs[-bad], -log10(ps[-bad]), xlab = "chromosomal location", 
+        ylab = "-log10 p Ho:Bs=0", main = ssr@gene, xlim=range(snpMeta[,"pos"]),        pch=pch, cex=cex)
     data(geneLocs)
     x = geneLocs[geneLocs$gene == ssr@gene, ]
     if (nrow(x) == 0) 
---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+
+.First.lib <- function(lib, pkg) {
+   library.dynam("mreg", pkg, lib)
+   }
+
