437a1510dc4264328f0a55c755feba8e22d03782
Vince Carey <stvjc@channing.harvard.edu>
2007-03-14 19:54:38 +0000

    major change to chr20GGdem and genoString to get rarebase data in and to avoid assuming named vectors
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@23196 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/genoString.R
+++ R/genoString.R
@@ -1,7 +1,13 @@
 genoStrings = function(racExSet, rsnum) {
- rac = snps(racExSet)[rsnum,]
- rar = rarebase(racExSet)[rsnum]
- gt = SNPalleles(racExSet)[rsnum]
+#
+# this assumes that rarebase is named -- and it is not required to
+# be.  so get an index
+#
+ if (length(rsnum)>1) stop("sorry, only handling a single snp id at present")
+ wh = which(snpNames(racExSet) == rsnum)
+ rac = snps(racExSet)[wh,]
+ rar = rarebase(racExSet)[wh]
+ gt = SNPalleles(racExSet)[wh]
  gts = as.character(strsplit(gt,"/")[[1]])
  com = gts[gts!=rar]
  opts = c( homrar=paste(rar,rar,sep="/"), het=as.character(gt), homcom=paste(com,com,sep="/"))
