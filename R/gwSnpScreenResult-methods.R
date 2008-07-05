66dff06415bb7dc7ae8043c206e8b8b5acbfe211
Vince Carey <stvjc@channing.harvard.edu>
2008-07-05 11:55:11 +0000

    improve topSnps
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@32572 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/gwSnpScreenResult-methods.R
+++ R/gwSnpScreenResult-methods.R
@@ -68,7 +68,7 @@ setMethod("topSnps", "gwSnpScreenResult", function(x, n=10, which="p.1df") {
    DF = 1
    if (which == "p.2df") DF = 2
    else if (which  != "p.1df") warning("chisq df assumed to be 1, 'which' not recognized")
-   pp = p.value(x@.Data[[1]], DF)
+   pp = p.value(w, DF)
    spp = pp[ order(pp, decreasing=FALSE) ]
    df = data.frame(p.val=spp)
    rownames(df) = names(spp)
