7508408b0b1bed847d1c99406ae6ade1db078473
Vince Carey <stvjc@channing.harvard.edu>
2008-10-05 13:10:29 +0000

    hbTestResults class introduced
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@34218 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/snpm2geno.R
+++ R/snpm2geno.R
@@ -50,18 +50,45 @@ bbHapTests = function(ph, cnum, sm, rsid, rad=1e5, doPhase=FALSE, ... ) {
   list(schaidRun=schaidRun, phap = ph)
 }
 
+setClass("hbTestResults", representation(call="call", hscoreOut="list",
+   locs="numeric", formula="ANY", cnum="chrnum", start="numeric", 
+   end="numeric", inc="numeric", smlSetName="character",
+   blocks="list", loci="list", initSNPs="list"))
+setGeneric("pvals", function(x) standardGeneric("pvals"))
+setMethod("pvals", "hbTestResults", function(x) {
+ sapply(x@hscoreOut, function(x)x$score.global.p)
+})
+setMethod("show", "hbTestResults", function(object) {
+ cat("GGtools haplotype block test results.\n")
+ cat("Model specified as", object@formula, "on", object@smlSetName, "\n")
+ cat("using locations", object@start, "to", object@end, "on chr", object@cnum, "\n")
+ cat("Blocks checked in intervals of length", object@inc, "\n")
+ cat("There are", length(object@hscoreOut), "test results.\n")
+ cat("Minimum global score p:", min(pvals(object)), "\n")
+})
+ 
+
 setGeneric("hbTests", function(fmla, sms, cnum, start, end, inc, ...) 
  standardGeneric("hbTests"))
 setMethod("hbTests", c("genesym", "smlSet", "chrnum", "numeric", "numeric", "numeric"),
   function(fmla, sms, cnum, start, end, inc, ...) {
+    theCall = match.call()
     if (length(cnum)!=1) stop("must have only a length-1 cnum")
     restr = sms[cnum,]
     ph = as.numeric(exprs(restr[fmla,]))
     sm = smList(restr)[[1]]
     locs = seq(start, end, inc)
 print(locs)
-    ans = lapply(locs, function(x) {gc(); print(x); bbHapTests(ph, cnum, sm, x+inc/2, inc/2, doPhase=FALSE, ...)})
-    list(runs=ans, locs=locs)
+    ans = lapply(locs, function(x) {gc(); print(x); 
+       bbHapTests(ph, cnum, sm, x+inc/2, inc/2, doPhase=FALSE, ...)})
+    loci = lapply(ans, function(x) x[[1]]$loci)
+    blocks = lapply(ans, function(x) x[[1]]$blocks)
+    initSNPs = lapply(ans, function(x) x[[1]]$initSNPs)
+    hscoreOut = lapply(ans, function(x) x[[1]]$hapscore)
+    tmp = list(runs=ans, locs=locs)
+    new("hbTestResults", call=theCall, hscoreOut=hscoreOut,
+     locs=locs, formula=fmla, cnum=cnum, start=start, end=end, inc=inc,
+     smlSetName=deparse(substitute(sms)), blocks=blocks, loci=loci, initSNPs=initSNPs)
 })
-  
+
   
