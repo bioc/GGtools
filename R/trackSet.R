9eb9e7f842b486dc8d81f2ff23e2893106c2cad4
Michael Lawrence <lawremi@iastate.edu>
2008-12-11 00:02:03 +0000

    Update GGtools in response to changes in rtracklayer. trackSet method becomes coercion method from cwSnpScreenResult to RangedData. No longer depends on rtracklayer; explicitly import IRanges.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@35829 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/trackSet.R
+++ R/trackSet.R
@@ -1,27 +1,20 @@
+setAs("cwSnpScreenResult", "RangedData", function(from) {
+  allp = p.value(from@.Data[[1]]) # , 1) # assume 1df -- must improve
+  rs = names(allp)
+  locstr = snpLocs.Hs(chrnum(from@chrnum), rsid(rs))
+  loc = locstr["loc",]
+  locrs = paste("rs", locstr["rsid",], sep="")
+  allp = allp[locrs]
+  
+  require(org.Hs.eg.db, quietly=TRUE)
+  rmap = revmap(org.Hs.egSYMBOL)
+  ch = paste("chr", from@chrnum, sep="")
 
-setMethod("trackSet", "cwSnpScreenResult", function(object, ...) {
-    allp = p.value(object@.Data[[1]]) # , 1) # assume 1df -- must improve
-    rs = names(allp)
-    locstr = snpLocs.Hs(chrnum(object@chrnum), rsid(rs))
-    loc = locstr["loc",]
-    locrs = paste("rs", locstr["rsid",], sep="")
-    allp = allp[locrs]
- 
-    require(org.Hs.eg.db, quietly=TRUE)
-    rmap = revmap(org.Hs.egSYMBOL)
-    ch = paste("chr", object@chrnum, sep="")
-
-    fdata = data.frame(chrom=ch, start=loc, end=loc, strand="+", phase=NA,
-       type="snpeff", group="gws", score=-log10(allp))
-    bad = which(is.na(allp) | !is.finite(allp))
-    if (length(bad) > 0) {
-     adata = assayDataNew("lockedEnvironment", dataVals=matrix(-log10(allp[-bad]),nc=1))
-     fd = new("AnnotatedDataFrame", data=fdata[-bad,])
-    } else {
-     adata = assayDataNew("lockedEnvironment", dataVals=matrix(-log10(allp),nc=1))
-     fd = new("AnnotatedDataFrame", data=fdata)
-    }
-    require(rtracklayer, quietly=TRUE)
-    new("trackSet", genome="Hs", assayData=adata, featureData=fd)
+  rd = RangedData(IRanges(loc, loc), type = "snpeff", group = "gws",
+    score = -log10(allp), space = ch, universe = "hg18")
+  allp = rd$score # order probably has changed
+  bad = is.na(allp) | !is.finite(allp)
+  if (any(bad))
+    rd = rd[!bad,]
+  rd
 })
-
