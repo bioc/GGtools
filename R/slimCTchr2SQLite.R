70811ebc418b740fd1944422f79eaaeb218e719c
Vince Carey <stvjc@channing.harvard.edu>
2009-11-23 03:00:27 +0000

    flat added
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@43212 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/slimCTchr2SQLite.R
+++ R/slimCTchr2SQLite.R
@@ -26,3 +26,29 @@ slimCTchr2SQLite = function(path,chr=1, dbname=paste("chr", chr, sep="")) {
 }
  
 
+
+slimCTchr2flat = function(path,chr=1, dbname=paste("chr", chr, sep="")) {
+ if (!is.numeric(chr)) stop("chr must be numeric index (for Hs, in 1-24)")
+ owd = getwd()
+ on.exit(setwd(owd))
+ tst = try(setwd(path))
+ if (inherits(tst, "try-error")) stop("could not change to given folder")
+ fn = dir(patt="\\+.rda$")
+ obn = gsub(".rda", "", fn)
+ load(fn[1])
+ rsn = get(obn[1])$snpnames[[chr]]
+ stats = get(obn[1])$chisq[[chr]]
+ rm(list=obn[1])
+ if (length(obn)>1) for (i in 2:length(obn))  {
+   load(fn[i])
+   stats = cbind(stats, get(obn[i])$chisq[[chr]])
+   rm(list=obn[i])
+   }
+ stats = cbind(rsid=rsn,stats)
+ dr = dbDriver("SQLite")
+ write.table(stats, paste(dbname, ".txt", sep=""))
+ cat("table written\n")
+ NULL
+}
+ 
+
