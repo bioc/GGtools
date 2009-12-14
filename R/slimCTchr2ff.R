e36f91eb31021c6ab5a0f184a96763fb0053f420
Vince Carey <stvjc@channing.harvard.edu>
2009-12-14 20:10:59 +0000

    using the ff package to deal with S x G structures
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@43554 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/slimCTchr2ff.R
+++ R/slimCTchr2ff.R
@@ -27,3 +27,36 @@ slimCTchr2ff = function (path, chr = 1, dbname = paste("chr", chr, sep = ""))
     cat("table written\n")
     NULL
 }
+
+ffs2minp = function(ffl, ...) {
+ if (!(is(ffl, "list"))) stop("ffl must be a list of ff objects")
+ nrs = sapply(ffl, nrow)
+ ncs = sapply(ffl, ncol)
+ if (!all(ncs == ncs[1])) stop("ffl members must have common ncol, check sapply(ffl, ncol)")
+ nff = length(ffl)
+ if (nff>1) {
+  comrs = ffl[[1]][,1]
+  for (i in 2:nff) {
+   comrs = intersect(comrs, ffl[[i]][,1])
+   }
+  clnffl = lapply( ffl, function(x) x[ match(comrs, x[,1]), ] )
+  plop1 = function(x){nm=sum(is.na(x));x[is.na(x)] = rchisq(nm,1);x}
+  clnffl = lapply( clnffl, plop1)
+  clnffl
+  summ =  clnffl[[1]]
+  for (i in 2:nff) {
+    summ = summ + clnffl[[i]]
+    }
+  }
+ else {
+  comrs = ffl[[1]][,1]
+  summ = ffl[[1]]
+  }
+ allmax = apply(summ[,-1],1,max)
+ # next line, use max for max chisq yield minp for fixed df
+ minind = apply(summ[,-1],1, function(x) {tmp = which.max(x); if(length(tmp)==0)tmp=NA;tmp}) # wm can return null
+ ans = pmin(1,2*(1-pchisq(allmax,nff)))
+ list(rsid = comrs, ans = ans, minind = minind,
+        genes = colnames(ffl[[1]])[-1])
+}
+
