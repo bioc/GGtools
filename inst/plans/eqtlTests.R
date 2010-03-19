399132cc35611e00322ff7bb7979c3fa6040c781
Vince Carey <stvjc@channing.harvard.edu>
2010-03-19 14:45:00 +0000

    one file per chrom
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@45345 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/plans/eqtlTests.R
+++ inst/plans/eqtlTests.R
@@ -10,21 +10,24 @@ eqtlTests = function(smlSet, rhs=~1-1,
  system(paste("mkdir", targdir))
  cres = chromApply( chrNames, function(chr) {
    snpdata = smList(smlSet)[[chr]]
+   #targff = paste( fnhead, "chr", chr, "_", "g", gene, ".ff" , sep="" )
+   targff = paste( fnhead, "chr", chr, ".ff" , sep="" )
    snpnames = colnames(snpdata)
    nsnps = ncol(snpdata)
+   store = ff( initdata=0, dim=c(nsnps, ngenes), dimnames=list(snpnames, geneNames), vmode="short",
+                 filename = targff )
    geneApply( geneNames, function(gene) {
-     targff = paste( fnhead, "chr", chr, "_", "g", gene, ".ff" , sep="" )
-#     store = ff( initdata=0, dim=c(nsnps, 1), dimnames=c(snpnames, gene), vmode="short",
-#                 filename = targff)
      ex = exprs(smlSet)[gene,]
      fmla = formula(paste("ex", as.character(rhs), collapse=" "))
      numans = snp.rhs.tests(fmla, snp.data=snpdata, data=pData(smlSet), family="gaussian", ...)@chisq
      miss = is.na(numans)
      if (any(miss)) numans[which(miss)] = rchisq(length(which(miss)), 1)
-     ff( initdata=shortfac*numans, dim=c(nsnps, 1), dimnames=list(snpnames, gene), vmode="short",
-                 filename = targff )
+     store[, gene, add=TRUE] = shortfac*numans
+     NULL
      }) # end gene apply
+  store
   })  # end chr apply
+  names(cres) = chrNames
   cres
 }
      
