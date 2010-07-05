76382726a372b138ba7f91dd869b9cbf138b649e
Vince Carey <stvjc@channing.harvard.edu>
2010-07-05 09:49:51 +0000

    evade a segfault with IRanges::%in% cropping up in tests/eqtest.R
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@47915 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/X2chunk.R
+++ R/X2chunk.R
@@ -13,13 +13,22 @@ X2chunk = function( mgr, ffind, start, end, snplocs, anno, useSym=TRUE ) {
   else stop("mgr must inherit from multffManager or eqtlTestsManager")
   snpNames = rownames(FFL[[ffind]])
   gNames = colnames(FFL[[ffind]])
-    sn2keep_inds = IRanges::"%in%"(ranges(snplocs)[[1]], lim)
+    #print(ranges(snplocs)[[1]])
+    #print(lim)
+    indsWithin = function(gr, lim) {
+      which(start(gr)>=start(lim) & end(gr)<= end(lim))
+    }
+    #print(summary(indsWithin(ranges(snplocs)[[1]], lim)))
+    #print(summary(ranges(snplocs)[[1]] %in% lim))
+    #sn2keep_inds = which(IRanges::"%in%"(ranges(snplocs)[[1]], lim))
+    sn2keep_inds = indsWithin( ranges(snplocs)[[1]], lim)
   if (length(sn2keep_inds)==0) stop("start:end too restrictive, no SNP")
   snpInRange = snplocs[ sn2keep_inds, ]
   snp2get = snpInRange[ order(start(snpInRange)), ]$name
   snp2get = intersect(snp2get, snpNames)
   gr = geneRanges( gNames, anno )
-    g2keep_inds = IRanges::"%in%"(ranges(gr)[[1]], lim)
+    #g2keep_inds = which(IRanges::"%in%"(ranges(gr)[[1]], lim))
+    g2keep_inds = indsWithin(ranges(gr)[[1]], lim)
   g2get = gr[g2keep_inds, ]$name
   ans = as.ram(FFL[[ffind]][snp2get, g2get])/SF
   if (useSym) {
