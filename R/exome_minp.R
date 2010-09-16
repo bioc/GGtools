19a616c283d737af46d22838170270ea6f33571e
Vince Carey <stvjc@channing.harvard.edu>
2010-09-16 03:28:01 +0000

    inor
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@49418 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/exome_minp.R
+++ R/exome_minp.R
@@ -1,4 +1,4 @@
-exome_minp = function( smlSet, fmla, targdir, runname, snpl, mgr=NULL, ... ) {
+exome_minpORIG = function( smlSet, fmla, targdir, runname, snpl, mgr=NULL, ... ) {
   if (is.null(mgr) && 
        length(smList(smlSet))>1) stop("requires a chromosome-unified smlSet [length smList == 1]")
   if (!is.null(mgr) && length(mgr@fflist)>1) stop("mgr must have fflist of length 1")
@@ -10,3 +10,28 @@ exome_minp = function( smlSet, fmla, targdir, runname, snpl, mgr=NULL, ... ) {
   names(ans) = names(snpl)
   ans
 }
+
+
+exome_minp = function (smlSet, fmla, targdir, runname, snpl, feat = NULL,
+    mgr = NULL, ...)
+{
+    if (is.null(mgr) && length(smList(smlSet)) > 1)
+        stop("requires a chromosome-unified smlSet [length smList == 1]")
+    if (!is.null(mgr) && length(mgr@fflist) > 1)
+        stop("mgr must have fflist of length 1")
+    if (is.null(mgr))
+        mgr = eqtlTests(smlSet, fmla, targdir = targdir, runname = runname,
+            ...)
+    if (is.null(feat))
+        scores = lapply(1:length(snpl), function(x) mgr[rsid(snpl[[x]]),
+            probeId(names(snpl)[x])])
+    else scores = lapply(1:length(snpl), function(x) mgr[rsid(snpl[[x]]),
+        probeId(feat)])
+    names(scores) = names(snpl)
+    ans = sapply(scores, function(x) sapply(x, function(w) apply(w,
+        2, function(z) min(1 - pchisq(z, mgr@df)))))
+    ans = as.numeric(ans)
+    names(ans) = names(snpl)
+    ans
+}
+
