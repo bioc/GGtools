8e611d081698dd5a4fd85a5e3a809eceac7e101f
Vince Carey <stvjc@channing.harvard.edu>
2010-10-22 10:50:54 +0000

    enhancing scoresInRanges to propagate names and to filter genes or chromosomes
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@50392 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/scoresInRanges.R
+++ R/scoresInRanges.R
@@ -30,19 +30,43 @@ containedSnps = function(geneRanges, snpRanges) {
   ans
 }
 
-scoresInRanges = function( mgr, geneRanges, snpRanges, applier=lapply ) {
+#scoresInRanges = function( mgr, geneRanges, snpRanges, applier=lapply ) {
+# snm = unlist(lapply(mgr@fflist, rownames))
+# iniRangeNames = names(snpRanges)
+# onboard = intersect(snm, iniRangeNames)
+# mm = match(onboard, iniRangeNames)
+# snpRanges = snpRanges[ mm ] # shld be faster than intersect(snm, names(snpRanges)) ]
+# snps = containedSnps( geneRanges, snpRanges )
+# gn = names(geneRanges)
+# applier( 1:length(snps), function(x) mgr[ rsid(snps[[x]]), probeId(gn[x]) ])
+#}
+ 
+
+scoresInRanges = function (mgr, geneRanges, snpRanges, applier = lapply, ffind=NULL) 
+{
  #
  # mgr is an eqtlTestsManager instance
  # geneRanges will typically be a GRanges extended for 'cis'
  # snpRanges can be a general snp metadata GRanges
+ #  oct 22-- introduced ffind to reduce scope of mgr to be examined if geneRanges is limited to one chr
+ #           also added filtering of geneRanges to genes available in mgr
  #
- snm = unlist(lapply(mgr@fflist, rownames))
- iniRangeNames = names(snpRanges)
- onboard = intersect(snm, iniRangeNames)
- mm = match(onboard, iniRangeNames)
- snpRanges = snpRanges[ mm ] # shld be faster than intersect(snm, names(snpRanges)) ]
- snps = containedSnps( geneRanges, snpRanges )
- gn = names(geneRanges)
- applier( 1:length(snps), function(x) mgr[ rsid(snps[[x]]), probeId(gn[x]) ])
+    pn = probeNames(mgr)
+    gonboard = names(geneRanges)
+    gok = match(pn, gonboard, nomatch=0 )
+    gok = gok[gok>0]
+    geneRanges = geneRanges[ gok ]
+    if (is.null(ffind)) snm = unlist(lapply(mgr@fflist, rownames))
+    else snm = unlist(lapply(mgr@fflist[ffind], rownames))
+    iniRangeNames = names(snpRanges)
+    onboard = intersect(snm, iniRangeNames)
+    mm = match(onboard, iniRangeNames)
+    snpRanges = snpRanges[mm]
+    snps = containedSnps(geneRanges, snpRanges)
+    gn = names(geneRanges)
+    ans = applier(1:length(snps), function(x) mgr[rsid(snps[[x]]), 
+        probeId(gn[x])])
+    names(ans) = names(geneRanges)
+    ans
 }
- 
+
