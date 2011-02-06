b421fde577d5d6123cc7795eae8ad0aea1206486
Vince Carey <stvjc@channing.harvard.edu>
2011-02-06 18:10:14 +0000

    trans experiments
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@52511 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/transTops.R
@@ -0,0 +1,9 @@
+
+transTops = function(mgr, chrind=22, n=10) {
+  ranksNvals = ffapply( X=mgr@fflist[[chrind]], AFUN=function(x)c(order(x,decreasing=TRUE)[1:n],
+         sort(x,decreasing=TRUE)[1:n]),
+       MARG=1, N=2*n, RETURN=TRUE, FF_RETURN=FALSE, CFUN="list")
+  if (length(ranksNvals)>1) ranksNvals = do.call("cbind", ranksNvals) else ranksNvals = ranksNvals[[1]]
+  list(ranks=ranksNvals[1:n,,drop=FALSE], vals=ranksNvals[-c(1:n),,drop=FALSE])
+}
+
