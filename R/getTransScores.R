c17e6aaaf6a23184301b159fabfdd083eaddb107
Vince Carey <stvjc@channing.harvard.edu>
2011-03-28 16:46:47 +0000

    improving trans function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@54099 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getTransScores.R
+++ R/getTransScores.R
@@ -3,7 +3,7 @@ tname = function() gsub(".*/", "", tempfile())
 cisTo = function (sr, gr, rad = 2e+06) 
 {
     ac = function(x) as(x, "character")
-    if (length(unique(seqnames(sr))) > 1) stop("need sr to represent only one chromosome")
+    if (length(unique(ac(seqnames(sr)))) > 1) stop("need sr to represent only one chromosome")
     if (!all(unique(ac(seqnames(sr))) ==  unique(ac(seqnames(gr))))) {warning("sr and gr represent different chromosomes"); return(NULL)}
     rope = sr + rad
     fo = findOverlaps(rope, gr)
@@ -16,7 +16,7 @@ cisTo = function (sr, gr, rad = 2e+06)
     ans
 }
 
-getTransScores = function(snpname, mgr, cisToList=NULL, ffind=1, n=200, searchfac=4 ) {
+getTransScoresOneSNP = function(snpname, mgr, cisToList=NULL, ffind=1, n=200, searchfac=4 ) {
 #
 # one SNP, same-chromosome application
 # cisToList has one element per SNP, names of probes within a given radius (use "cisTo" to generate)
@@ -31,8 +31,13 @@ getTransScores = function(snpname, mgr, cisToList=NULL, ffind=1, n=200, searchfa
  if(!is.null(cisToList)) drops = intersect(cisToList[[sn]], names(t1))
  if (length(drops)==0) return(sort(t1, decreasing=TRUE)[1:n])
  t1 = t1[ -match(drops, names(t1)) ]
- sort(t1, decreasing=TRUE)[1:min(c(length(t1),n))]
+ ot1 = order(t1, decreasing=TRUE)[1:min(c(length(t1),n))]
+ sco1 = t1[ot1]
+ n1 = names(t1)[ot1]
+ list(scores=sco1, genes=n1)
 }
+
+#getTransScores = function(mgr, cisToList=NULL, ffind=1, 
 
 #if (!exists("t1")) load("t1.rda")
 #gg = getTransScores( rsid("rs4814683"), mgr=t1, cisToList=fff )
