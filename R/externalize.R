a6e27583ffa22e5fa558e0190901fe0151c4cdf9
Vince Carey <stvjc@channing.harvard.edu>
2011-04-28 00:50:38 +0000

    wrapperEndo added to getSS
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@55111 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/externalize.R
+++ R/externalize.R
@@ -53,7 +53,8 @@ externalize = function(smlSet, packname, author="Replace Me <auth@a.b.com>",
  NULL
 }
 
-getSS = function( packname, chrs, renameChrs=NULL, probesToKeep=NULL ) {
+getSS = function( packname, chrs, renameChrs=NULL, probesToKeep=NULL,
+   wrapperEndo=NULL ) {
  if (!is.null(renameChrs) && (length(chrs) != length(renameChrs)))
    stop("renameChrs must have same length as chrs in call to getSS")
  require(packname, character.only=TRUE)
@@ -70,7 +71,12 @@ getSS = function( packname, chrs, renameChrs=NULL, probesToKeep=NULL ) {
  sml = lapply(chrs, function(x) get(load(paste(partsfol, "/", x, ".rda", sep=""))))
  if (is.null(renameChrs)) names(sml) = chrs
  else names(sml) = renameChrs
- make_smlSet( ex, sml, harmonizeSamples=TRUE )
+ ans = make_smlSet( ex, sml, harmonizeSamples=TRUE )
+ if (is.null(wrapperEndo)) return(ans) else {
+   ans = wrapperEndo(ans)
+   if (isTRUE(tst <- validObject(ans))) return(ans)
+   stop(tst)
+ }
 }
 
 #hmceuB36 = new("extSmlSet", packname="ceuhm2", chrnames=
