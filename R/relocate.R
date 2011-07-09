e5ab6d16fd9502b42124ac72a4003873d451710d
Vince Carey <stvjc@channing.harvard.edu>
2011-07-09 09:34:50 +0000

    improved relocate functionality for summary preservation
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@56644 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/relocate.R
+++ R/relocate.R
@@ -1,9 +1,17 @@
 
 relocate = function(old, new, obj, ffind=1) {
+# do the fflist component
  inst = obj@fflist[[ffind]]
  fref = attr(attributes(inst)[["physical"]], "filename")
  ans = gsub(old, new, fref)
  attr(attributes(inst)[["physical"]], "filename") = ans
  obj@fflist[[ffind]] = inst
+# do the summary component (MAF, RAF)
+ inst = obj@summaryList[[ffind]]
+ fref = attr(attributes(inst)[["physical"]], "filename")
+ ans = gsub(old, new, fref)
+ attr(attributes(inst)[["physical"]], "filename") = ans
+ obj@summaryList[[ffind]] = inst
+# return
  obj
 }
