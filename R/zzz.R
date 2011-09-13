e7768032d36da5e39a4bca2802f923ef4a4b9e2a
Vince Carey <stvjc@channing.harvard.edu>
2011-09-13 04:11:17 +0000

    a startup message noting the replacement of data(hmceuB36.2021) functionality
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@57999 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+.onLoad = function(libname, pkgname) {
+ packageStartupMessage(cat("\n---as of Sept 2011, data(hmceuB36.2021) is removed\n",
+    "---in favor of getSS('GGtools', c('20', '21'))\n"))
+}
