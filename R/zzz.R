c9acfd4c9de0f307256acb67bdb86dc900ad8feb
Vince Carey <stvjc@channing.harvard.edu>
2011-09-13 15:12:01 +0000

    minor aesthetic change
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@58014 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onLoad = function(libname, pkgname) {
- packageStartupMessage(cat("\n---as of Sept 2011, data(hmceuB36.2021) is removed\n",
+ packageStartupMessage(cat("\n ---as of Sept 2011, data(hmceuB36.2021) is removed\n",
     "---in favor of getSS('GGtools', c('20', '21'))\n"))
 }
