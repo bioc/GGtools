b1960824e2585757ebd49b7b6409ead35d6dffdd
Vince Carey <stvjc@channing.harvard.edu>
2011-09-20 20:44:48 +0000

    fix to sorted output of observed scores in fdrtab object
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@58248 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/cisFDR.R
+++ R/cisFDR.R
@@ -18,46 +18,6 @@ cat("elements are:\n", names(objects))
 cat("\n")
 })
 
-.genewiseFDRtab = function(sms, rhs, nperm=1, seed=1234, targp=c(.95, .975, .99, .995),
-	folderstem="fdrf", geneApply=mclapply, gene2snpList=NULL) {
-  set.seed(seed)
-  if (nperm > 1) stop("not supporting more than 1 perm at present")
-  pm = featureNames(sms)
-  obs = eqtlTests(sms, rhs, geneApply=geneApply, targdir=folderstem)
-  per = eqtlTests(permEx(sms), rhs, geneApply=geneApply, 
-	targdir=paste("p", folderstem, sep=""))
-  if (is.null(gene2snpList)) {
-    tops = unlist(geneApply(pm, function(x)topFeats(probeId(x), mgr=obs, ffind=1, 
-		n=1)))
-    ptops = unlist(geneApply(pm, function(x)topFeats(probeId(x), mgr=per, ffind=1, 
-		n=1)))
-    }
-  else {
-    gn = intersect(names(gene2snpList), pm)
-    if (length(gn) < 1) stop("gene2snpList is non-null and has null intersection with featureNames(sms)")
-    tops = sapply(1:length(gn), function(z) max(as.numeric(unlist(obs[rsid(gene2snpList[[gn[z]]]),
-        probeId(gn[z]) ]))))
-    ptops = sapply(1:length(gn), function(z) max(as.numeric(unlist(per[rsid(gene2snpList[[gn[z]]]),
-        probeId(gn[z]) ]))))
-    }
-  nullq = quantile(ptops, targp)
-  fcalls = sapply(nullq, function(x)sum(ptops>x))
-  scalls = sapply(nullq, function(x)sum(tops>x))
-  fdrtab = cbind(pctile=100*targp, thres=nullq, nfalse=fcalls, nsig=scalls, fdr=fcalls/scalls)
-  sotops = sort(tops, decreasing=TRUE)
-  sptops = sort(ptops, decreasing=TRUE)
-  sfdr = sapply(sotops, function(x) sum(sptops>=x)/sum(sotops>=x))
-  nf = sfdr*length(sfdr)
-  ncall = 1:length(sfdr)
-  nc01 = min(which(sfdr >= .01))
-  nc05 = min(which(sfdr >= .05))
-  nc10 = min(which(sfdr >= .10))
-  new("eqtlFDRtab", list(fdrtab=fdrtab, obsmgr=obs, permmgr=per, 
-	universe=pm, tops=tops, permtops=ptops,
-     	nullq = nullq, targp=targp, ncall=ncall, sfdr=sfdr,
-	nc01=nc01, nc05=nc05, nc10=nc10))
-}
-
 policyFDRtab = function(sms, rhs, universe=featureNames(sms),
   policyClo=function(mgr) function(x)topFeats(probeId(x),
      mgr=mgr, ffind=1, n=1), nperm=1, seed=1234, targp=c(.95, .975, .99, .995),
@@ -167,9 +127,12 @@ genewiseFDRtab = function(sms, rhs, nperm=1, seed=1234, targp=c(.95, .975, .99, 
  nc01 = min(which(sfdr >= .01))
  nc05 = min(which(sfdr >= .05))
  nc10 = min(which(sfdr >= .10))
+ nc12.5 = min(which(sfdr >= .125))
+ nc15 = min(which(sfdr >= .15))
  new("eqtlFDRtab", list(fdrtab=fdrtab, obsmgr=obs, permmgr=perlist, 
-	universe=obs$pm, sorted.tops=obs$tops, sorted.av.permtops=sptops,
+	universe=obs$pm, sorted.tops=obs$sotops, sorted.av.permtops=sptops,
         nsnpsmgd = obs$nsnpsmgd, nprobes=obs$nprobes, nsnptests=obs$nsnptests,
      	nullq = nullq, targp=targp, ncall=ncall, sfdr=sfdr,
-	nc005=nc005, nc01=nc01, nc05=nc05, nc10=nc10, thecall=thecall))
+	nc005=nc005, nc01=nc01, nc05=nc05, nc10=nc10, nc12.5=nc12.5,
+        nc15=nc15, thecall=thecall))
 }
