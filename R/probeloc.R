fb83179ea3b8f87076c4c40139384a4ef8d0664a
Vince Carey <stvjc@channing.harvard.edu>
2011-10-29 02:40:38 +0000

    fix up wih elementLengths
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@59844 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/probeloc.R
+++ R/probeloc.R
@@ -204,7 +204,7 @@ setSoloSeq = function(tx, ind) {
       ponc = ponc[-badeg]
       }
    txok = reduce(tx[egids])
-   alll = sapply(txok, length)
+   alll = elementLengths(txok)
    if (any(alll != 1)) {
      warning("some entrez genes selected in TxDb had disjoint transcripts; picking first for addressing")
      txok = txok[-which(alll != 1)]
