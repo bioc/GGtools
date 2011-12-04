d030e4ec9949221e600f4fc81aa3774604f556c7
Vince Carey <stvjc@channing.harvard.edu>
2011-12-04 18:24:11 +0000

    improvements to reporting
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@61109 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/comb.R
+++ R/comb.R
@@ -2,14 +2,34 @@
 setClass("eqtlFDRSummary", representation(
   allpermtops="numeric", obsrd="GRanges", calls="list",
   theCall="call", sess="ANY", genome="character", cisRadius="numeric",
-  nperm="numeric"))
+  nperm="numeric", nat05="numeric", nat01="numeric"))
+
+setGeneric("fullreport", function(x) standardGeneric("fullreport"))
+setMethod("fullreport", "eqtlFDRSummary", function(x)
+ x@obsrd)
 
 setMethod("show", "eqtlFDRSummary", function(object) {
  nr = length(object@obsrd)
- cat("eqtlTestsSummary instance.  Best observed features (of", nr, 
+ cat("eqtlTestsSummary instance.  Best observed features (of ", nr, 
     "):\n", sep="")
- todo = min(c(nr, 3))
- print(object@obsrd[1:todo])
+ todo = min(c(nr, 4))
+ allans = object@obsrd
+ toshow = object@obsrd[1:todo]
+ print(data.frame(probe=names(toshow), 
+  chr=as.character(seqnames(toshow)),
+  codeStart=start(toshow),
+  codeEnd=end(toshow), rsid=elementMetadata(toshow)$rsid,
+  snploc=elementMetadata(toshow)$snploc, 
+  `chisq(1)`=elementMetadata(toshow)$chisq, check.names=FALSE))
+#  `-log10fdr`=elementMetadata(toshow)$score, check.names=FALSE))
+ cat("Plug-in FDR based on", object@nperm, "permutations.\n")
+ nat01 = sum(elementMetadata(allans)$score >= 2)
+ nat05 = sum(elementMetadata(allans)$score >= -log10(0.05))
+ cat("There were", nat01, "calls at FDR=0.01;", nat05, "at FDR=0.05.\n", sep=" ")
+ cat("call was:\n")
+ print(object@theCall)
+ cat("===\n")
+ cat("use fullreport() for information on all probes tested.\n")
 })
 
 setMethod("c", "eqtlFDRtab", function(x, ..., recursive=FALSE) {
@@ -31,5 +51,8 @@ setMethod("c", "eqtlFDRtab", function(x, ..., recursive=FALSE) {
  names(elementMetadata(rd)) = nn
  lsfdr = ifelse(sfdr==0, 16, -log10(sfdr))
  elementMetadata(rd)$score = lsfdr
- new("eqtlFDRSummary", allpermtops=alp, obsrd=rd, calls=calls)
+ nat01 = sum(elementMetadata(rd)$score >= 2)
+ nat05 = sum(elementMetadata(rd)$score >= -log10(0.05))
+ new("eqtlFDRSummary", allpermtops=alp, obsrd=rd, calls=calls,
+          nat01=nat01, nat05=nat05)
 })
--- R/pcc.R
+++ R/pcc.R
@@ -1,7 +1,7 @@
   pcChooser = function(sms, cand=c(1,10,15,20,25,30,40), fmla, radius=c(100000), chr="20", smlc="20", 
     geneApply=lapply, pvals=c(1e-6,1e-7,1e-8,1e-9), ncore=NULL, ffind=1, ...) {
  # strictly Cis testing at this time 7 Jan 2011
-   if (!is.null(ncore)) options(cores=ncore)
+   if (!is.null(ncore)) options(cores=ncore,mc.cores=ncore)
    if (length(radius) > 1) stop("radius should have length 1")
    require(sms@annotation, character.only=TRUE)
    anpref = gsub(".db", "", sms@annotation)
