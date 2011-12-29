dbe48af197f0306a30c69304f1d7bfe96f8a7723
Vince Carey <stvjc@channing.harvard.edu>
2011-12-29 03:26:11 +0000

    allow combination of eqtlFDRSummary with c()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@61599 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/comb.R
+++ R/comb.R
@@ -1,6 +1,6 @@
 
 setClass("eqtlFDRSummary", representation(
-  allpermtops="numeric", obsrd="GRanges", calls="list",
+  allpermtops="numeric", obsrd="GRanges", calls="list", nperm="numeric",
   theCall="call", sess="ANY", genome="character", cisRadius="numeric",
   nperm="numeric", nat05="numeric", nat01="numeric"))
 
@@ -36,7 +36,7 @@ setMethod("c", "eqtlFDRtab", function(x, ..., recursive=FALSE) {
  if (recursive) stop("recursive mode not implemented")
  args = unname(list(x, ...))
  cls = sapply(args, class)
- if (!all(cls[1] == cls)) stop("all args to c for eqtlTestsManager must have same class")
+ if (!all(cls[1] == cls)) stop("all args to c for eqtlFDRtab must have same class")
  alp = unlist(lapply(args, function(x) x$sorted.all.permtops))
  rd = do.call(c, lapply(args, function(x) x$gro))
  np = do.call(c, lapply(args, function(x) x$nperm))
@@ -53,6 +53,28 @@ setMethod("c", "eqtlFDRtab", function(x, ..., recursive=FALSE) {
  elementMetadata(rd)$score = lsfdr
  nat01 = sum(elementMetadata(rd)$score >= 2)
  nat05 = sum(elementMetadata(rd)$score >= -log10(0.05))
- new("eqtlFDRSummary", allpermtops=alp, obsrd=rd, calls=calls,
+ new("eqtlFDRSummary", allpermtops=alp, obsrd=rd, calls=calls, nperm=nperm,
+          nat01=nat01, nat05=nat05)
+})
+
+setMethod("c", "eqtlFDRSummary", function(x, ..., recursive=FALSE) {
+ if (recursive) stop("recursive mode not implemented")
+ args = unname(list(x, ...))
+ cls = sapply(args, class)
+ if (!all(cls[1] == cls)) stop("all args to c for eqtlFDRSummary must have same class")
+ np = do.call(c, lapply(args, function(x) x@nperm))
+ if (var(np)>0) stop("nperm must be constant over all summaries")
+ nperm = np[1]
+ alp = unlist(lapply(args, function(x) x@allpermtops))
+ rd = do.call(c, lapply(args, function(x) x@obsrd))
+ rd = rd[order(elementMetadata(rd)$chisq, decreasing=TRUE)]
+ obstops = elementMetadata(rd)$chisq
+ sfdr = sapply(obstops, function(x) (sum(alp > x)/nperm)/max(c(1,sum(obstops>x))))
+ calls = do.call(c, lapply(args, function(x) x@calls))
+ lsfdr = ifelse(sfdr==0, 16, -log10(sfdr))
+ elementMetadata(rd)$score = lsfdr
+ nat01 = sum(elementMetadata(rd)$score >= 2)
+ nat05 = sum(elementMetadata(rd)$score >= -log10(0.05))
+ new("eqtlFDRSummary", allpermtops=alp, obsrd=rd, calls=calls, nperm=nperm,
           nat01=nat01, nat05=nat05)
 })
