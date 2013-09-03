ab833072a263e00911523f04e0aebc8675b3888a
Vince Carey <stvjc@channing.harvard.edu>
2013-09-03 19:12:24 +0000

    starting cleanup
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@80009 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/harvest.R
+++ R/harvest.R
@@ -1,5 +1,5 @@
 
-cis.FDR.filter.best = function( fn, 
+cis.FDR.filter.best.old = function( fn, 
     hi.dist = 50000, low.dist = -Inf, hi.maf = .51, low.maf = 0.05,
     fdrOnly = FALSE, applier=lapply ) {
 #
@@ -238,4 +238,62 @@ cis.FDR.filter.SNPcentric.complete = function( fn,
      } 
   data.frame(genes=unlist(ngs), snps=alls, chr=allchr, fdr=pp,
      scores=rawscores)
+}
+
+
+cis.FDR.filter.best = function (fn, hi.dist = 50000, low.dist = -Inf, hi.maf = 0.51, 
+    low.maf = 0.05, fdrOnly = FALSE, applier = lapply) 
+{
+#
+# see above for comments
+#
+    require(GGtools)
+    if (!exists(gsub(".rda", "", fn[1]))) 
+        objs = lapply(fn, function(x) get(load(x, .GlobalEnv)))
+    else objs = lapply(gsub(".rda", "", fn), get)
+    nperm = length(grep("permScore", names(values(objs[[1]]))))
+    cf = function(x) cisFilter(x, hi.dist = hi.dist, low.dist = low.dist, 
+        hi.maf = hi.maf, low.maf = low.maf)
+    chrtags = sapply(objs, function(x) as.character(seqnames(x)[1]))
+    bs = lapply(objs, function(x) {
+        cat(".")
+        bestInStratum(cf(x))
+    })
+    bss = lapply(bs, "[[", "scores")
+    realmafs = lapply(objs, function(x) data.frame(snp=x$snp, probeid=x$probeid, MAF=x$MAF, stringsAsFactors=FALSE))
+    library(parallel)
+    ps = applier(1:nperm, function(x) lapply(objs, function(z) {
+        cat(".")
+        bestInStratum(cf(z), permind = x)
+    }))
+    pss = lapply(ps, function(x) lapply(x, function(z) z[["scores"]]))
+    rawscores = unlist(bss)
+    pp = pifdr(rawscores, unlist(pss))
+    ng = sapply(bs, function(x) length(x[[1]]))
+    allg = unlist(lapply(bs, function(x) names(x[[1]])))
+    if (fdrOnly) {
+        names(pp) = allg
+        return(pp)
+    }
+    alls = unlist(lapply(bs, "[[", "scorerids"))
+    allchr = rep(chrtags, ng)
+
+# fix 9/3/13 to handle multiple files in fn
+    tmp = lapply(ps, function(x) lapply(x, 
+        "[[", 1))
+    tmp = lapply(tmp, unlist)
+    allpbest = do.call(cbind,tmp)
+
+    colnames(allpbest) = paste0("permScore_", 1:ncol(allpbest))
+    bestmafs = vector("list", length(bs))
+    for (i in 1:length(bs)) {
+      tagi.best = paste( names(bs[[i]][[1]]), bs[[i]][["scorerids"]], sep=":" )
+      tagi.maf = paste(realmafs[[i]]$probeid, realmafs[[i]]$snp, sep=":" )
+      kp = match( tagi.best, tagi.maf )
+      bestmafs[[i]] = realmafs[[i]][kp, "MAF"]
+      }
+    ans = data.frame(genes = allg, bestsnp = alls, chr = allchr, 
+        fdr = pp, scores = rawscores, MAF.observed=unlist(bestmafs))
+    cbind(ans, allpbest)
+#    ans
 }
