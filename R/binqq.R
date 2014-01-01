c67de37aaba26b157b3cfa7c72f798bd9ba7c09e
Vince Carey <stvjc@channing.harvard.edu>
2014-01-01 06:02:56 +0000

    various improvements for end-to-end
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@85180 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/binqq.R
+++ R/binqq.R
@@ -7,15 +7,15 @@ qqp = qqplot( sco, c(p1, p2, p3) , plot.it=FALSE)
 list(hb=hexbin( qqp$x, qqp$y, xbins=nxbins ), thrs=thrs, scothrs=unlist(scothrs) )
 }
 
-binqq = function(  qqob, ylim=c(0, 76), xlim=c(0, 30),  ... ) {
- par(mar=c(5,4,4,4))
+binqq = function(  qqob, ylim=c(0, 76), xlim=c(0, 30), end45=5,  ... ) {
+# par(mar=c(5,4,4,4))
  plot( qqob$hb@ycm, qqob$hb@xcm , pch= " ", xlab="Permuted", ylab="Observed",
      ylim=ylim, xlim=xlim )
  #points( qqob$hb@ycm, qqob$hb@xcm , pch= 19, col="gray")
  ok = which(qqob$hb@xcm < qqob$scothrs[1] )
  text( qqob$hb@ycm[ok], qqob$hb@xcm[ok] , labels=qqob$hb@count[ok], pch=.4 )
  text( qqob$hb@ycm[length(ok)+1], qqob$hb@xcm[length(ok)+1] , labels=sum(qqob$hb@count[-ok]), pch=.4 )
- segments( 0, 0, 5, 5, lwd = 3 )
+ segments( 0, 0, end45, end45, lwd = 3 )
  abline(h=qqob$scothrs, lty=1:length(qqob$scothrs) )
  axis(4, at=qqob$scothrs, labels=qqob$thrs, cex.axis=.75, las=2 )
 }
--- R/gffprocess.R
+++ R/gffprocess.R
@@ -38,5 +38,83 @@ gffprocess = function(basename="fullyri100k", n_in=44, headpatt="_1A", tmpForSor
  NULL
 }
  
-
  
+simpleTiling = function(ntile) {
+ require(Homo.sapiens)
+ hsi = seqinfo(Homo.sapiens)[paste0("chr", 1:22),]
+ GenomicRanges::unlist(tileGenome(hsi, ntile=100))
+ }
+
+cgff2dt = function(gff3, tiling ) {
+#
+# this program takes a genome-wide gff3 assembled by gffprocess after ciseqByCluster
+# and a) assembles a data.table instance with the same content, b) computes the genome-wide FDR,
+# c) builds a revised gff3 with gw
+#
+  require(foreach)
+  require(Rsamtools)
+  stopifnot(is(tiling, "GRanges"))
+  basen = gsub(".gff3.gz", "", gff3)
+  th = headerTabix(gff3)
+  orderedChr = th$seqnames
+  lgr = foreach(i=1:length(tiling)) %dopar% {
+    gc()
+    cat(i)
+    lk = try(import.gff3( gff3, which=tiling[i] ))
+    if (inherits(lk, "try-error")) lk = NULL
+    if (!is.null(lk)) lk = as.data.table(as.data.frame(lk))
+    lk
+  }
+  bad = sapply(lgr, is.null)
+  if (any(bad)) lgr = lgr[-which(bad)]
+  ans = do.call(rbind, lgr)
+  ans$snplocs = as.numeric(ans$snplocs)
+  ans$ests = as.numeric(ans$ests)
+  ans$se = as.numeric(ans$se)
+  ans$oldfdr = as.numeric(ans$fdr)
+  ans$MAF = as.numeric(ans$MAF)
+  ans$dist.mid = as.numeric(ans$dist.mid)
+  nperm = length(grep("permS", names(ans)))
+  pnames = paste("permScore_", 1:nperm, sep="")
+  for (i in 1:nperm)
+    ans[[ pnames[i] ]] = as.numeric( ans[[ pnames[i] ]] )
+  ans$mindist = as.numeric(ans$mindist)  # 
+print(date())
+  ans$fdr = pifdr(ans$score, c(ans$permScore_1, ans$permScore_2, ans$permScore_3))
+print(date())
+  obn = paste0(basen, "_dt")
+  assign(obn, ans)
+  save(list=obn, file=paste0(obn, ".rda"))
+# prepare a file with the properly ordered genome-wide FDR
+ gwfdr = ans$fdr
+  gwch = ans$seqnames
+  gwsnp = ans$snp
+  gwprobeid = ans$probeid
+#
+# following code was used to verify synchronization to gff upon selecting by orderedChr
+#
+#  newdt = data.frame(gwch, gwsnp, gwprobeid, gwfdr, stringsAsFactors=FALSE)
+#  newdt = split(newdt, gwch)
+#  reviseFDR = file("newfdr.csv", open="a")
+#  for (i in 1:length(orderedChr)) {
+#      write.table(newdt[newdt$gwch == orderedChr[i],], file=reviseFDR,col.names=FALSE,row.names=FALSE,sep=",")
+#  }
+#  close(reviseFDR)
+#
+# use unix utilities to add the genome-wide FDR to the gff3
+  sgwfdr = split(gwfdr, gwch)
+  sgwfdr = unlist(sgwfdr[orderedChr])
+
+    nn = tempfile()
+    fdrt = file(nn, "w")
+
+  writeLines(c(c(" ", " ", " "), paste("; gwfdr=", round(sgwfdr,6), sep="")), con=fdrt)
+  close(fdrt)
+
+  chkb = system(paste0("zcat ", gff3, " | paste -d '' - ", nn, " | bgzip > ", gsub(".gff3", "wmlf.gff3", gff3)))
+  if(!(chkb == 0)) warning(paste("final system zcat-paste-bgzip returned non-zero: chkb=", chkb ))
+  invisible(chkb)
+
+}
+
+
