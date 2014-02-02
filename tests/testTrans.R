bdd89400ae6b1d11e25aa58977c5ed0d499d4ac3
Vince Carey <stvjc@channing.harvard.edu>
2014-02-02 14:10:29 +0000

    use configuration approach in transScores
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@85971 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testTrans.R
+++ tests/testTrans.R
@@ -5,17 +5,33 @@ tenOn2021 =
 c( "GI_4557248-S",  "GI_15451784-S", "GI_4557678-S", "GI_9951914-S", "GI_21327679-S",
  "GI_7669476-I", "GI_7669478-A", "GI_4557290-I", "GI_4557294-A", "GI_41406053-S") 
 
+tconf = new("TransConfig")
+radius(tconf) = 2000000L
+smpack(tconf) = "GGdata"
+rhs(tconf) = ~1
+snpchr(tconf) = "22"
+chrnames(tconf) = c("20", "21")
+gbufsize(tconf) = 4
+smFilter(tconf) = function(x) x[probeId(tenOn2021),]
+snpannopk(tconf) = snplocsDefault()
+schrpref(tconf) = "ch"
+exFilter(tconf) = function(x)x
+gchrpref = ""
+batchsize(tconf) = 200L
+
 suppressPackageStartupMessages(library(GGtools))
 
-t1 = transScores("GGdata", rhs=~1, snpchr="22", chrnames=as.character(c(20,21)),
+t1 = transScores(tconf)
+
+#"GGdata", rhs=~1, snpchr="22", chrnames=as.character(c(20,21)),
 #
 # all tests are trans, to verify buffering approach
 #
-        radius = 2e+06,  K=4,
-    probesToKeep = tenOn2021, batchsize = 200, 
-    geneannopk = "illuminaHumanv1.db", 
-    snpannopk = "SNPlocs.Hsapiens.dbSNP.20111119", gchrpref = "", 
-    schrpref = "ch", exFilter = function(x) x) 
+#        radius = 2e+06,  K=4,
+#    probesToKeep = tenOn2021, batchsize = 200, 
+#    geneannopk = "illuminaHumanv1.db", 
+#    snpannopk = "SNPlocs.Hsapiens.dbSNP.20111119", gchrpref = "", 
+#    schrpref = "ch", exFilter = function(x) x) 
 
 tt1 = transTab(t1)
 
