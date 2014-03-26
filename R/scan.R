3c9f4afa4a916524a098fa78177cfdb283e6ddd7
Vince Carey <stvjc@channing.harvard.edu>
2014-03-26 01:03:32 +0000

    an experimental full trans for one gene
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGtools@87901 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/scan.R
@@ -0,0 +1,20 @@
+eqtlscan = function( 
+    infmla=`GI_23397697-A`~1, smpack="GGdata", chrtag="20", ...) {
+#
+# decode formula 
+#
+    respObj = infmla[[2]] # we know infmla is a formula, infmla[[2]] is dep var
+#
+# at this point we have the featureName that we need
+#
+    pname = as.character(respObj)
+    infmla[[2]] = as.name(pname)  # replace the dependent variable spec in fmla
+    data(eset, package=smpack) # defines ex
+    gtdat = get(load(system.file(paste("parts/", chrtag, ".rda", sep="", collapse=""),
+      package=smpack)))
+    assign(pname, exprs(ex)[pname,]) # expression vector
+    alld = data.frame(get(pname), pData(ex))
+    names(alld)[1] = pname
+    snp.rhs.tests(infmla, family="gaussian",
+        snp.data=gtdat, data=alld, uncertain=TRUE)
+}
